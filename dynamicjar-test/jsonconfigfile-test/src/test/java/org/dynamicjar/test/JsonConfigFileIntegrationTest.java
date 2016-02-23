6d2372bd8cb90992dcf325c72f80a87a05a06b41
erikhakansson <erikhakan@gmail.com>
2016-02-23 16:17:02 +0100

    Added json config file test

--- dynamicjar-test/jsonconfigfile-test/src/test/java/org/dynamicjar/test/JsonConfigFileIntegrationTest.java
+++ dynamicjar-test/jsonconfigfile-test/src/test/java/org/dynamicjar/test/JsonConfigFileIntegrationTest.java
@@ -13,7 +13,7 @@ import static org.testng.Assert.assertEquals;
  * Created by Erik Håkansson on 2016-02-15.
  * Copyright 2016
  */
-public class YamlConfigFileIntegrationTest {
+public class JsonConfigFileIntegrationTest {
 
     private static final String LOG_4_J_CLASS_NAME = "org.apache.logging.log4j.core.Logger";
 
@@ -28,8 +28,8 @@ public class YamlConfigFileIntegrationTest {
     public void loadJarIntegrationTest()
         throws DependencyResolutionException, ClassNotFoundException, IllegalAccessException,
         InstantiationException, PropertyLoadException {
-        DynamicJar.loadDependencies(YamlConfigFileTestTarget.class);
-        YamlConfigFileTestTarget business = new YamlConfigFileTestTarget();
+        DynamicJar.loadDependencies(JsonConfigFileTestTarget.class);
+        JsonConfigFileTestTarget business = new JsonConfigFileTestTarget();
         business.helloWorld();
         assertEquals(business.getLogger().getClass().getName(), LOG_4_J_CLASS_NAME);
     }
