c22b8f7b299552b7f3d419c3208c2782f0895500
Erik H <erikhakan@gmail.com>
2017-04-12 20:49:10 +0200

    Bumped version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ import org.gradle.script.lang.kotlin.repositories
 
 allprojects {
   group = "io.squark.yggdrasil"
-  version = "0.0.1-SNAPSHOT"
+  version = "0.2.0-SNAPSHOT"
 
   repositories {
     gradleScriptKotlin()
